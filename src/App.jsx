e6f63ca54526aacfe3bebef2fe5f72a4b1ababfe
Arslonbek <roziboyevarslon0110@gmail.com>
2024-05-18 19:30:32 +0500

    counter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import { useState } from "react"
-import city from './assets/img_one.jpeg'
-import "./App.css"
 function App(){
+  // const [count,setCount] = useState(0)
+  // const [step,setStep] = useState(0)
+  const [counter,setCounter] = useState([])
+  const addCounter =(item)=>{
+    counter.push(item)
+    setCounter([...counter])
+  }
+  const increaseItem =(index)=>{
+    counter[index] += 1
+    setCounter([...counter])
+  }
+  const decreaseItem =(index)=>{
+    counter[index] -= 1
+    setCounter([...counter])
+  }
   return (
     <>
      <div className="container">
       <div className="row mt-5">
         <div className="col-md-6 offset-3 text-center">
-         <h1>xatolikni togirlang</h1>
-         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum, modi culpa? Laborum quibusdam, cupiditate ipsum veritatis praesentium dolorem rerum fuga et doloribus quidem, dolore dolorum! Sequi architecto natus delectus illum.
-         </p>
+         {/* <h1>Count: {count}</h1>
+         <h1>Step: {step}</h1>
+          <button className="btn btn-success m-3" onClick={()=>setStep(prev => prev + 1)}>step</button>
+          <button className="btn btn-info m-3" onClick={()=>setCount(prev => prev + step)}>count</button> */}
+          <button onClick={()=>addCounter(1)}>add counter</button>
+          {
+            counter.map((item,index)=>{
+              return (
+                <div key={index}>
+                <button onClick={()=>increaseItem(index)}>+</button>
+                <span>{item}</span>
+                <button onClick={()=>decreaseItem(index)}>-</button>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
      </div>
